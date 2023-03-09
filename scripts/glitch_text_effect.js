2bd9a3055141b711a89937352c9a83dc2b2ec462
Morshok <78026935+Morshok@users.noreply.github.com>
2023-03-09 20:31:13 +0100

    Added text glitch effect on hover in index.html

---
+++ scripts/glitch_text_effect.js
@@ -0,0 +1,23 @@
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const interval_time_ms = 30;
+
+document.querySelector("#glitch_text").onmouseover = event => {
+    let iteration = 0;
+    
+    const interval = setInterval(() => {
+        event.target.innerText = event.target.innerText.split("")
+        .map((letter, index) => {
+            if(iteration < index) {
+                return event.target.dataset.value[index];
+            }
+            return alphabet[Math.floor(Math.random() * alphabet.length)]);
+        }
+        .join("");
+    }, interval_time_ms);
+    
+    if(iteration >= event.target.dataset.value.length) {
+        clearInterval(interval);
+    }
+    
+    iteration += 1 / 2;
+}
